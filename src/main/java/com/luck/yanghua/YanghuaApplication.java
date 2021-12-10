15bc7b3cb32406999c3e9434075ff99d60ebc73b
ayzq1582 <15010071819@163.com>
2021-12-10 17:39:09 +0800

    提交代码

---
+++ src/main/java/com/luck/yanghua/YanghuaApplication.java
@@ -0,0 +1,12 @@
+package com.luck.yanghua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanghuaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YanghuaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/luck/yanghua/YanghuaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luck.yanghua;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YanghuaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
